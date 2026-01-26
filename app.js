d9f81679667b89c19f9c695e47b2e330d59ac7ef
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-01-26 06:41:58 -0700

    test succeeded

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello developer");
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello,developer");
+});
+
+app.listen(port, () => {
+  console.log(`test successfully, the port is ${port}`);
+});
