d0265340026a56d24fe9e00e92aaeeb002696dad
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-01-26 07:37:55 -0700

    get jokes finished

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 "use strict";
 
 require("dotenv").config();
-
 const express = require("express");
 const app = express();
 const db = require("./db");
 
 const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("hello,developer");
-// });
-
-app.get("api");
+app.get("/api/jokes", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT * FROM jokes");
+    res.json(rows);
+  } catch (err) {
+    console.error("DB error full", err);
+    res
+      .status(500)
+      .json({ error: "DB error", code: err.code, message: err.message });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`test successfully, the port is ${port}`);
+  console.log(`On port ${port}`);
 });
--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 "use strict";
 
-const mysql = reuqire("mysql2/promise");
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "xouyang_xouyang_joke_admin",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "xouyang_joke_share",
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   charset: "utf8mb4",
